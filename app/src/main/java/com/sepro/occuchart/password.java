5f4896841318c262635f5404668deadd8525d733
crack.vr <visweshram11@gmail.com>
2020-03-15 16:47:48 +0530

    passcode

---
+++ app/src/main/java/com/sepro/occuchart/password.java
@@ -0,0 +1,39 @@
+package com.sepro.occuchart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.hanks.passcodeview.PasscodeView;
+
+public class password extends AppCompatActivity {
+    // initialize variable
+    PasscodeView passcodeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password);
+
+        // assign variable
+        passcodeView = findViewById(R.id.passcodeView1);
+
+        passcodeView.setPasscodeLength(4)
+                .setLocalPasscode("5678")
+                .setListener(new PasscodeView.PasscodeViewListener() {
+                    @Override
+                    public void onFail() {
+                        Toast.makeText(getApplicationContext()
+                        ,"Password is Wrong",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onSuccess(String number) {
+                        Intent intent = new Intent(MainActivity.this,)
+
+                    }
+                })
+    }
+}
