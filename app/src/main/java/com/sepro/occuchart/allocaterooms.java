c002b14682ecd164a30ee539f6b9d8eb3b831456
Hirito <hariharanvenkey@gmail.com>
2020-03-15 23:34:35 +0530

    closing app from main activity

--- app/src/main/java/com/sepro/occuchart/allocaterooms.java
+++ app/src/main/java/com/sepro/occuchart/allocaterooms.java
@@ -1,15 +1,21 @@
 package com.sepro.occuchart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,6 +39,8 @@ public class allocaterooms extends AppCompatActivity {
                 CollectionReference colrefroom = db.collection("rooms");
                 CollectionReference colrefcourse = db.collection("courses");
                 CollectionReference colrefttable = db.collection("timetable");
+
+
 
             }
         });
